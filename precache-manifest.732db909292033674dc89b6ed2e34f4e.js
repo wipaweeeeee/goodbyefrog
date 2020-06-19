4bb7a27687eee18e9cfbf550b869399b078fbd19
Wipawe Sirikolkarn <wsirikolkarn@gmail.com>
2020-06-19 02:08:07 -0400

    Updates

---
+++ precache-manifest.732db909292033674dc89b6ed2e34f4e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e823e828130cb99298676a7c0154c9b4",
+    "url": "/goodbyefrog/index.html"
+  },
+  {
+    "revision": "18fbd18df0ab1626bc29",
+    "url": "/goodbyefrog/static/css/main.8cf6af44.chunk.css"
+  },
+  {
+    "revision": "29f9c09a40691a5152c8",
+    "url": "/goodbyefrog/static/js/2.c9917a6f.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/goodbyefrog/static/js/2.c9917a6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18fbd18df0ab1626bc29",
+    "url": "/goodbyefrog/static/js/main.46974623.chunk.js"
+  },
+  {
+    "revision": "39344000d2f2f92aab2a",
+    "url": "/goodbyefrog/static/js/runtime-main.68506e82.js"
+  },
+  {
+    "revision": "cf5048791449a8c29d340b63f5f67ba5",
+    "url": "/goodbyefrog/static/media/TitlingGothicFBExtendedBold.cf504879.otf"
+  },
+  {
+    "revision": "b462828d0281101a2869401687c30e9e",
+    "url": "/goodbyefrog/static/media/TitlingGothicFBNormalMedium.b462828d.otf"
+  },
+  {
+    "revision": "ac0814dc8c64b0a865ac8e776596296e",
+    "url": "/goodbyefrog/static/media/disp.ac0814dc.jpeg"
+  },
+  {
+    "revision": "c139449aa2ef82be05d56dd2e2207ebc",
+    "url": "/goodbyefrog/static/media/frog_0.c139449a.png"
+  },
+  {
+    "revision": "826d0dbc383081798646265509e031b4",
+    "url": "/goodbyefrog/static/media/frog_1.826d0dbc.png"
+  },
+  {
+    "revision": "8a55f06e9820e92ae5ba9bfed9e88e56",
+    "url": "/goodbyefrog/static/media/frog_2.8a55f06e.png"
+  },
+  {
+    "revision": "531aef4ae81171bd9dce8e0acab7a7d0",
+    "url": "/goodbyefrog/static/media/frog_3.531aef4a.png"
+  },
+  {
+    "revision": "908d7b3862e376d819ceaa2744eafdb7",
+    "url": "/goodbyefrog/static/media/frog_4.908d7b38.png"
+  },
+  {
+    "revision": "ddf048fe71ebad72d6ec867775b1b5d1",
+    "url": "/goodbyefrog/static/media/frog_5.ddf048fe.png"
+  },
+  {
+    "revision": "7b740738087b094249af28bb42656d0b",
+    "url": "/goodbyefrog/static/media/frog_6.7b740738.png"
+  }
+]);
